c33544cb78c197b615b5f3a00ce28961f492accc
Adnan <adnanvandyck@gmail.com>
2024-01-22 07:06:28 -0500

    set up starter code from replit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import UserInterface from './UserInterface';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <main>
+        <h1
+        style={{
+          color: "#FEE001",
+          fontFamily: "Luminari, fantasy",
+          fontSize: 50
+        }}
+        >
+          Welcome to Comics Galore!
+        </h1>
+        <UserInterface />
+      </main>
     </div>
   );
 }
---
+++ src/RequestFormButton.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+
+function RequestFormButton() {
+  return (
+    <div>
+        <button>
+            Click Here!
+        </button>
+        <input type="submit" value="Submit" />
+    </div>
+  )
+}
+
+export default RequestFormButton
